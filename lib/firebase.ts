389f06313e3db03d1e574e8a44a347987a975c52
BalajiKundapura <Balaji.kundapuras@gmail.com>
2025-10-16 17:55:04 -0500

    main

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBe3GdbgTpv7N4SX0mNYzMdFMS3KehUbFI",
   authDomain: "neuroshine-1892b.firebaseapp.com",
   projectId: "neuroshine-1892b",
-  storageBucket: "neuroshine-1892b.firebasestorage.app",
+  storageBucket: "neuroshine-1892b.appspot.app",
   messagingSenderId: "12640584551",
   appId: "1:12640584551:web:4aeaf8dd071e2b12afe5c2",
 }
