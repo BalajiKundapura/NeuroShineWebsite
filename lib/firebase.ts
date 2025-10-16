84d98d7225d5305c1fd1753e7ba853abbb8e2241
BalajiKundapura <Balaji.kundapuras@gmail.com>
2025-10-16 18:00:05 -0500

    stuff

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,9 +6,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyBe3GdbgTpv7N4SX0mNYzMdFMS3KehUbFI",
   authDomain: "neuroshine-1892b.firebaseapp.com",
   projectId: "neuroshine-1892b",
-  storageBucket: "neuroshine-1892b.appspot.app",
+  storageBucket: "neuroshine-1892b.firebasestorage.app",
   messagingSenderId: "12640584551",
   appId: "1:12640584551:web:4aeaf8dd071e2b12afe5c2",
+  measurementId: "G-M1SVHEMWSC"
 }
 
 // Initialize Firebase (singleton pattern)
